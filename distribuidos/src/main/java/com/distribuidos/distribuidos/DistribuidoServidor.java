960c273f018a7775770cfc0afca0e539a6d0f40c
Erick Freitas <85972033+erickelsas@users.noreply.github.com>
2025-06-20 11:54:19 -0300

    tempo

--- distribuidos/src/main/java/com/distribuidos/distribuidos/DistribuidoServidor.java
+++ distribuidos/src/main/java/com/distribuidos/distribuidos/DistribuidoServidor.java
@@ -12,6 +12,9 @@ public class DistribuidoServidor {
             int end = 10000;
             int mid = (start + end) / 2;
 
+            // Início da medição de tempo
+            long tempoInicio = System.currentTimeMillis();
+
             Map<String, List<String>> result1 = worker1.analyzeNumbers(start, mid);
             Map<String, List<String>> result2 = worker1.analyzeNumbers(mid + 1, end);
 
@@ -24,11 +27,17 @@ public class DistribuidoServidor {
             allFriendlies.addAll(result1.get("friendlies"));
             allFriendlies.addAll(result2.get("friendlies"));
 
+            // Fim da medição de tempo
+            long tempoFim = System.currentTimeMillis();
+            long duracao = tempoFim - tempoInicio;
+
             System.out.println("Numeros perfeitos encontrados:");
             allPerfects.forEach(System.out::println);
 
             System.out.println("\nPares de numeros amigaveis encontrados:");
             allFriendlies.forEach(System.out::println);
+
+            System.out.println("\nTempo de processamento: " + duracao + " ms");
 
         } catch (Exception e) {
             e.printStackTrace();
